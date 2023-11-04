7e4011bf621c3b95c7d6e7c02ab76496de2875c5
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-11-04 14:19:13 -0400

    revert to working prototype

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ import streamlit as st
 
 #Import dependencies
 from langchain.llms import GPT4All
+from langchain import PromptTemplate, LLMChain
 
-#Python imports
-from langchain_experimental.agents.agent_toolkits.create_python_agent import create_python_agent
-from langchain.tools.python.tool import PythonREPLTool
 # Path to weights
 PATH = '/Users/joshkelly/Library/Application Support/nomic.ai/GPT4All/mistral-7b-openorca.Q4_0.gguf'
 
-# LLM instance
 llm = GPT4All(model=PATH, verbose=True)
 
-# Python Agent
-python_agent = create_python_agent(llm-llm, tool=PythonREPLTool(), verbose=True)
-# Title
+# Prompt template
+prompt = PromptTemplate(input_variables=['question'],
+                        template="""
+                        Question: {question}
+                        
+                        Answer: Let's think that through step by step
+                        """)
 
+# LLM Chain
+chain = LLMChain(prompt=prompt, llm=llm)
+# Title
 st.title('AI-Project')
 
 # Prompt text box
@@ -24,6 +28,6 @@ prompt = st.text_input('Input prompt here')
 
 if prompt:
   #Pass prompt
-  res = python_agent.run(prompt)
+  res = chain.run(prompt)
 
   st.write(res)
